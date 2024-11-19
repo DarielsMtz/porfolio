be859c93737c121c4ae01b4da1a6ec1c89abc4b7
Dariels A. Valladares Martinez <darielsmartinez926@gmail.com>
2024-11-19 12:09:53 +0100

    Agregar nuevos SVGs de gradiente y textura, implementar animación de desplazamiento infinito en el componente Hero

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,11 +5,16 @@ export default {
     extend: {
       animation: {
         float: "float 6s ease-in-out infinite",
+        "infinite-scroll": "infinite-scroll 25s linear infinite",
       },
       keyframes: {
         float: {
           "0%, 100%": { transform: "translateY(0)" },
           "50%": { transform: "translateY(-10px)" },
+        },
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
         },
       },
     },
